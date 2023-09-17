05f463f091a13a16ec6a052986d5eb9c0a515811
unker <kfi.unknown@gmail.com>
2023-09-17 23:34:24 +0300

    Collapse works

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -4,31 +4,41 @@ export default class CollapsibleContainer {
   constructor(container, contentText) {
     this.container = container;
 
+    this.containerCollapse = document.createElement('div');
+    this.containerCollapse.classList.add('collapse-container');
+
     this.button = this.createCollapseButton();
+    this.content = this.createCollapseContent(contentText);
 
-    this.content = document.createElement('div');
-    this.content.classList.add('collapse-content');
-    this.content.innerText = contentText;
-    this.container.appendChild(this.content);
+    this.container.appendChild(this.containerCollapse);
 
-    console.log(this.content);
-
-
-    this.isCollapsed = false;
-
-    // Добавляем обработчик события для кнопки
     this.button.addEventListener('click', () => this.toggleCollapse());
-
   }
 
   createCollapseButton() {
     const button = document.createElement('button');
+    button.classList.add('collapse-button');
     button.innerText = 'Collapse';
-    this.container.appendChild(button);
+    this.containerCollapse.appendChild(button);
     return button;
+  }
+
+  createCollapseContent(contentText) {
+    const content = document.createElement('div');
+    const contentInner = document.createElement('div');
+    content.classList.add('collapse-content');
+    contentInner.classList.add('collapse-inner');
+    contentInner.innerText = contentText;
+    this.containerCollapse.appendChild(content);
+    content.appendChild(contentInner);
+    return content;
   }
 
   toggleCollapse() {
     this.content.classList.toggle('collapsed');
+  }
+
+  getDOMElement() {
+    return this.containerCollapse;
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,17 @@ import CollapsibleContainer from '../components/Collapse';
 
 const container = document.querySelector('.container');
 
-const myCollapsible = new CollapsibleContainer(container, 'Test text');
+const testText = `
+1
+2
+3
+4
+5
+`
+const collapsible = new CollapsibleContainer(container, testText);
+
+const collapsibleEl = collapsible.getDOMElement();
+
+const anotherContent = document.createElement('div');
+anotherContent.innerText = 'another content';
+collapsibleEl.appendChild(anotherContent);
